70c29773f17c199002ca6a06f8822f30b472d16a
mishaganin <gma-13@yandex.ru>
2021-10-01 11:03:50 +0300

    Simple header

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../header/header';
+import './app.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div></div>
+      </div>
+    )
+  }
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>ITMO Lab</h1>
+            <nav>
+                <a>Labs</a>
+                <a>Labs</a>
+                <a>Labs</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
