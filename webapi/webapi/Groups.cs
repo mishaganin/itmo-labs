28258bd514bdc8daf186996165947482df0253dc
Evgeniy <pesochin_evgeniy@mail.ru>
2021-10-28 11:06:55 +0300

    feat: create base API example

---
+++ webapi/webapi/Groups.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webapi
+{
+	public class Group
+	{
+		public string Name { get; set; }
+
+		public Student[] Students { get; set; }
+	}
+}
---
+++ webapi/webapi/Student.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace webapi
+{
+	public class Student
+	{
+		public string Name { get; set; }
+
+		public string Surename { get; set; }
+
+		public int ID { get; set; }
+
+	}
+}
